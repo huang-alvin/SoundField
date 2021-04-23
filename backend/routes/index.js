5fe5e38d54054a4209a5f4f21aa4fd492ef51e68
alvin <huang.alvin.01@gmail.com>
2021-04-23 14:17:51 -0700

    Login finished except for css

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -31,7 +31,6 @@ if (process.env.NODE_ENV === "production") {
 
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== "production") {
-  console.log("backend api/csrf/restore route");
   router.get("/api/csrf/restore", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
     return res.json({});
---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,66 @@
+import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+// import { login } from "../../store/session";
+import * as sessionActions from "../../store/session";
+import "./LoginForm.css";
+
+function LoginFormPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  if (sessionUser) {
+    return <Redirect to="/" />;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // this does 2 things
+    // 1. resetting the error array
+    // 2. triggers re-render after succesful submission which allows the user to be redirected
+    //      from the if statement above
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password })).catch(
+      async (res) => {
+        const data = await res.json();
+        console.log(data);
+        if (data && data.errors) setErrors(data.errors);
+      }
+    );
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <ul>
+        {errors.map((error, idx) => (
+          <li key={idx}>{error}</li>
+        ))}
+      </ul>
+      <label>
+        Username or Email
+        <input
+          type="text"
+          value={credential}
+          onChange={(e) => setCredential(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Log In</button>
+    </form>
+  );
+}
+export default LoginFormPage;
