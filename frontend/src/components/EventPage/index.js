80c9c4e9c959b886663b469244b55765bb41585a
alvin <huang.alvin.01@gmail.com>
2021-04-29 23:48:00 -0700

    buttons render, memo: need to utilize auth , and test backend api/events

--- frontend/src/components/EventPage/index.js
+++ frontend/src/components/EventPage/index.js
@@ -1,15 +1,89 @@
 import { useDispatch, useSelector } from "react-redux";
-// import { useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import { addOneBookmark } from "../../store/bookmark";
+import { addOneTicket } from "../../store/ticket";
 
 import "./EventPage.css";
 
 function EventPage() {
+  const [hasBookmark, setHasBookmark] = useState(false);
+  const [hasTicket, setHasTicket] = useState(false);
   const { eventId } = useParams();
+  const history = useHistory();
+  const dispatch = useDispatch();
 
   const event = useSelector((state) => state.events[parseInt(eventId, 10)]);
 
   const userId = useSelector((state) => state.session.user.id);
+  const userBookmarksArr = useSelector((state) =>
+    Object.values(state.bookmarks)
+  );
+  const userTicketsArr = useSelector((state) => Object.values(state.tickets));
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    history.push(`/events/edit/${event.id}`);
+  };
+  const handleBookmark = (e) => {
+    e.preventDefault();
+    dispatch(addOneBookmark(userId, event.id)); //i'm thinking i dont need to await. could be wrong
+    setHasBookmark(true);
+  };
+  const handleTicket = (e) => {
+    e.preventDefault();
+    dispatch(addOneTicket(userId, event.id));
+    setHasTicket(true);
+  };
+
+  useEffect(() => {
+    userBookmarksArr.forEach((bookmark) => {
+      if (bookmark.eventId === parseInt(eventId, 10)) setHasBookmark(true);
+    });
+    userTicketsArr.forEach((ticket) => {
+      if (ticket.eventId === parseInt(eventId, 10)) setHasTicket(true);
+    });
+  }, []);
+
+  let bookmarkButton;
+  if (hasBookmark) {
+    bookmarkButton = (
+      <div className="have-bookmark">This event is in your Bookmarks</div>
+    );
+  } else {
+    bookmarkButton = (
+      <button
+        className="bookmark-button"
+        type="button"
+        onClick={handleBookmark}
+      >
+        Add to my Bookmarks
+      </button>
+    );
+  }
+
+  let editButton;
+  if (event.userId === userId) {
+    editButton = (
+      <button type="button" className="edit-button" onClick={handleEdit}>
+        Edit
+      </button>
+    );
+  } else {
+    editButton = <div />;
+  }
+
+  let buyButton;
+  if (hasTicket) {
+    buyButton = <div className="has-ticket">Already have ticket</div>;
+  } else {
+    buyButton = (
+      <button className="ticket-button" onClick={handleTicket}>
+        Buy Ticket
+      </button>
+    );
+  }
 
   return (
     <div className="events-container">
@@ -20,7 +94,11 @@ function EventPage() {
         />
       </div>
       <div className="event-info-container">
-        <div className="button-container"></div>
+        <div className="button-container">
+          {bookmarkButton}
+          {editButton}
+          {buyButton}
+        </div>
         <div className="event-body">
           <div className="event-title">{event.title}</div>
           <div className="event-description">{event.description}</div>
--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -2,12 +2,14 @@ import { useDispatch, useSelector } from "react-redux";
 import React, { useState, useEffect } from "react";
 import { loadAllEvents } from "../../store/event";
 import { loadTickets } from "../../store/ticket";
+import { loadBookmarks } from "../../store/bookmark";
 function HomePage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
   useEffect(() => {
     dispatch(loadAllEvents(sessionUser.id));
     dispatch(loadTickets(sessionUser.id));
+    dispatch(loadBookmarks(sessionUser.id));
   }, [dispatch]);
   return <div>home</div>;
 }
